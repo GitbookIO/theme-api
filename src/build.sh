6ccd7c02f478062771a629bce4ec3b98fd4600eb
Johan Preynat <johan.preynat@gmail.com>
2016-05-18 13:25:38 +0200

    Update build script and remove unused browserify

--- src/build.sh
+++ src/build.sh
@@ -7,7 +7,7 @@ rm -rf assets
 mkdir -p assets
 
 # Compile JS
-browserify src/js/theme-api.js | uglifyjs -mc > assets/theme-api.js
+uglifyjs -mc -- src/js/theme-api.js > assets/theme-api.js
 
 # Compile Website CSS
 lessc -clean-css src/less/website.less assets/theme-api.css
