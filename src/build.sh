425e3624c7c96c9108c06f80614ce58d9fad6771
Johan Preynat <johan.preynat@gmail.com>
2016-05-18 09:44:41 +0200

    Adapt build script

--- src/build.sh
+++ src/build.sh
@@ -1,13 +1,4 @@
 #! /bin/bash
 
-# Cleanup folder
-rm -rf _assets
-
-# Recreate folder
-mkdir -p _assets/website/
-
-# Compile JS
-browserify src/js/theme/index.js | uglifyjs -mc > _assets/website/theme.js
-
 # Compile Website CSS
-lessc -clean-css src/less/website.less _assets/website/style.css
+lessc -clean-css src/less/website.less assets/theme-api.css
