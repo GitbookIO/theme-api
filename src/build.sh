8b57f8b65a2494edcf340bcbb4caf33ea2083615
Johan Preynat <johan.preynat@gmail.com>
2016-05-18 09:53:22 +0200

    Update build script

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
+# Cleanup folder
+rm -rf assets
+
+# Recreate folder
+mkdir -p assets
+
+# Compile JS
+browserify src/js/theme-api.js | uglifyjs -mc > assets/theme-api.js
+
 # Compile Website CSS
 lessc -clean-css src/less/website.less assets/theme-api.css
