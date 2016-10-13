76853deaecb2661f07e10f2bd52854dffaf6ebcd
Johan Preynat <johan.preynat@gmail.com>
2016-10-13 23:06:26 +0200

    Update server-side to return correct props for React

--- index.js
+++ index.js
@@ -1,58 +1,9 @@
-var _ = require('lodash');
-var Q = require('q-plus');
+var _       = require('lodash');
+var Promise = require('q-plus');
 var cheerio = require('cheerio');
 
 var DEFAULT_LANGUAGES = require('./languages');
 var configLanguages = [];
-
-function generateMethod(book, body, examples) {
-    // Main container
-    var $ = cheerio.load('<div class="api-method"></div>'),
-        $apiMethod = $('div.api-method'),
-    // Method definition
-        $apiDefinition = $('<div class="api-method-definition"></div>'),
-    // Method code
-        $apiCode = $('<div class="api-method-code"></div>');
-
-    // Append elements
-    $apiMethod.append($apiDefinition);
-    $apiMethod.append($apiCode);
-
-    // Render method body
-    return Q()
-    .then(function() {
-        return book.renderBlock('markdown', body);
-    })
-    .then(function(apiDefinition) {
-        $apiDefinition.html(apiDefinition);
-
-        // Set method examples
-        return Q(examples).eachSeries(function(example) {
-            var $example;
-
-            // Common text
-            if (example.type == 'common') {
-                $example = $('<div class="api-method-example"></div>');
-
-            }
-
-            // Example code snippet
-            if (example.type == 'sample') {
-                $example = $('<div class="api-method-sample" data-lang="'+example.lang+'" data-name="'+example.name+'"></div>');
-            }
-
-            return book.renderBlock('markdown', example.body)
-            .then(function(body) {
-                $example.html(body);
-                $apiCode.append($example);
-            });
-        });
-    })
-    .then(function() {
-        // Return whole HTML
-        return $.html('div.api-method');
-    });
-}
 
 module.exports = {
     book: {
@@ -67,42 +18,50 @@ module.exports = {
 
     blocks: {
         method: {
-            blocks: ['sample', 'common'],
-            process: function(blk) {
+            blocks: [ 'sample', 'common' ],
+            process: function(block) {
+                var book     = this;
                 var examples = [];
 
-                _.each(blk.blocks, function(_blk) {
+                return Promise(block.blocks).eachSeries(function(_block) {
                     var languageName;
 
                     // Search if is user-defined language
-                    if (_blk.name == 'sample') {
+                    if (_block.name == 'sample') {
                         // Sample blocks should have a lang argument
-                        if (!_blk.kwargs.lang) {
+                        if (!_block.kwargs.lang) {
                             throw Error('sample blocks must provide a "lang" argument');
                         }
 
-                        var language = _.find(configLanguages, { lang: _blk.kwargs.lang });
+                        var language = _.find(configLanguages, { lang: _block.kwargs.lang });
 
                         if (!!language) {
                             languageName = language.name;
                         } else {
                             // Default to upper-cased lang
-                            languageName = _blk.kwargs.lang.toUpperCase();
+                            languageName = _block.kwargs.lang.toUpperCase();
                         }
                     }
 
-                    examples.push({
-                        type: _blk.name,
-                        body: _blk.body.trim(),
-                        lang: _blk.kwargs.lang,
-                        name: languageName
+                    return book.renderBlock('markdown', _block.children.trim())
+                    .then(function(content) {
+                        examples.push({
+                            type:     _block.name,
+                            content:  content,
+                            language: _block.kwargs.lang,
+                            name:     languageName
+                        });
+                    });
+                })
+                .then(function() {
+                    return book.renderBlock('markdown', block.children.trim())
+                    .then(function(definition) {
+                        return {
+                            definition: definition,
+                            examples:   examples
+                        };
                     });
                 });
-
-                return {
-                    parse: true,
-                    body: generateMethod(this, blk.body.trim(), examples)
-                };
             }
         }
     },
